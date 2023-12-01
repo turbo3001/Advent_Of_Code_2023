610725c6009c429275d66a2ca54ab01b52633b0b
turbo3001 <lukerackstraw@hotmail.com>
2023-12-01 12:36:55 +0000

    Day 1: Part 2

--- Day_1/src/Calibration.py
+++ Day_1/src/Calibration.py
@@ -1,11 +1,38 @@
+PART2_MODE = True
+
+NUMBERS_AS_STRING = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+]
+
+def FindNumbersInString( inputLine ):
+    numbersInString = []
+    for index in range(len(inputLine)):
+        if( inputLine[index].isnumeric() ):
+            numbersInString.append( int(inputLine[index]) )
+            continue
+        if( PART2_MODE ):
+            for numberIndex in range( len( NUMBERS_AS_STRING ) ):
+                number = NUMBERS_AS_STRING[numberIndex].lower()
+                currentCheck = inputLine[ index : index + len( number ) ]
+                if( currentCheck == number ):
+                    numbersInString.append(numberIndex)
+                    index += len(number)
+    return numbersInString
+
 inputdata = open( '../InputData/input.txt', 'r' )
 sum = 0
 for inputLine in inputdata:
-    numbersInLine = []
-    for character in inputLine:
-        if( character.isnumeric() ):
-            numbersInLine.append( int(character) )
+    numbersInLine = FindNumbersInString( inputLine )
     lineNumber = ( numbersInLine[0] * 10 ) +  numbersInLine[-1]
     sum += lineNumber
-    print( f'{inputLine[:-1]} = {lineNumber} (running total: {sum})' )
+    print( f'{inputLine[:-1]}\n\tfound numbers { ', '.join( map( str, numbersInLine ) ) }\n\tso lineNumber is {lineNumber}\n\trunning total: {sum}' )
 print(f'The calibration value is {sum}')
