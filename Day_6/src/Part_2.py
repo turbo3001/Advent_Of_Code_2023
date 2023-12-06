7e1611b61e288a0992766a7379505deaf0006d8f
turbo3001 <lukerackstraw@hotmail.com>
2023-12-06 17:32:14 +0000

    Day 6: Part 2

---
+++ Day_6/src/Part_2.py
@@ -0,0 +1,35 @@
+class Race():
+    def __init__(self, time, distance) -> None:
+        self.time = int(time)
+        self.distance = int(distance)
+    def __str__(self) -> str:
+        return f'[time: {self.time}, distance: {self.distance}]'
+    def __repr__(self) -> str:
+        return str(self)
+    def GetWinningDistanceCount(self) -> int:
+        count = 0
+        i = 0
+        while i < self.time:
+            if i * (self.time - i) > self.distance:
+                count += 1
+            i = i + 1
+        return count
+
+def parseRaces( filename ) -> list[Race]:
+    races = []
+    raceInfo = {}
+    for row in open( filename, 'r' ):
+        splitRow = row.split( ':' )
+        raceInfo[ splitRow[0] ] = splitRow[1].replace(' ', '').replace('\n', '')
+
+    races.append( Race( raceInfo['Time'], raceInfo['Distance'] ) )
+    
+    return races
+
+races = parseRaces( '../InputData/RaceTimes.txt' )
+print(races)
+sum = 1
+for race in races:
+    sum *= race.GetWinningDistanceCount()
+
+print(sum)
